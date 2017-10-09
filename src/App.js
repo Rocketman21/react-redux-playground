44618d6e2dc4e9e5bb2aae2ade2e7a16eb0e128a
Стас <stas@TK-Admins-Mac-mini.local>
2017-10-09 18:51:15 +0300

    Split in two components with properties.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header name={"Azaza"} initialAge={23}>
+          testing children.
+        </Header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          { console.log('adasd')  } 
         </p>
+        
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 
 export class Header extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      age: props.initialAge
+    }
+  }
+
+  onMakeOlder() {
+    this.setState({
+      age: this.state.age + 1
+    });
+  }
+
   render() {
+    console.log(logo);
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
+        Name is {this.props.name}. He is {this.state.age} old. Children: {this.props.children}
+        <button onClick={() => this.onMakeOlder()}>Make him older</button>
       </header>
     );
   }
